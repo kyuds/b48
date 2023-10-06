2a2476459f6c8d9119f1d0f236b01bc0175e293f
Daniel Shin <kyuseung1016@gmail.com>
2023-10-06 20:22:44 +0900

    add board printing functionalities

--- 2048.sh
+++ 2048.sh
@@ -8,11 +8,15 @@ VER_CNT=3
 # SETTINGS
 SIZE=4 # creates a SIZE x SIZE grid for play
 
-# BASIC HELPERS
+# DRAWING HELPERS
 function init_board() {
     for ((i=0; i<$(expr "$SIZE" '*' "$SIZE"); i++)); do
         board[i]="1"
     done
+
+    board[2]=2048
+    board[14]=65536
+    board[5]=512
 }
 
 function draw_boarder_line() {
@@ -35,7 +39,55 @@ function draw_horizontal_empty() {
     echo "|"
 }
 
+# CALCULATIONS
+function ln() {
+    n="$1"
+    length=${#n}
+
+    # number exceeded board constraints
+    if [[ $length -gt $HOR_CNT ]]; then
+        win
+    fi
+}
+
+function win() {
+    # need to clear board
+    echo "You win!"
+    exit 0
+}
+
+# PRINT BOARD
+function print_board() {
+    draw_boarder_line
+    for ((r=0; r<$SIZE; r++)); do
+        draw_horizontal_empty
+	
+        for ((c=0; c<$SIZE; c++)); do
+            idx=$(expr "$r" '*' "$SIZE" '+' "$c")
+            itm=${board[idx]}
+            rightpad=$(( ($HOR_CNT - ${#itm}) / 2 ))
+            leftpad=$(( $HOR_CNT - $rightpad - ${#itm} ))
+            
+            echo -n "|"
+            for ((i=0; i<$leftpad; i++)); do
+                echo -n " "
+            done
+            echo -n "$itm"
+            for ((i=0; i<$rightpad; i++)); do
+                echo -n " "
+            done
+        done
+        echo "|"
+
+        draw_horizontal_empty
+        draw_boarder_line
+    done
+}
+
+# GAME LOGIC
+
 init_board
-draw_boarder_line
-draw_horizontal_empty
+print_board
+
+
 
